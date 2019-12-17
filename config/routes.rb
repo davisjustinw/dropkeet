bff4ac6126a2b21626b9e730cfbfffcc4899a22a
davisjustinw <davisjustinw@gmail.com>
2019-12-16 21:18:32 -0800

    add inventory_item nested resource to inventory

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-
-  resources :inventories, only: [:index, :show, :edit, :new, :create]
+  resources :inventories, only: [:index, :show, :edit, :new, :create] do
+      resources :inventory_items, only: [:show]
+  end
 
   resources :items, only: [:index, :new, :create, :edit, :update]
 
-  resources :inventory_items, only: [:edit, :update, :new, :create]
+  resources :inventory_items, only: [:edit, :update, :new, :create] do
+    resources :entries
+  end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers =>
+    { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   root "welcome#home"
-
 end
