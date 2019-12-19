31aa22adae126eb4ef0a4469f41aeb2a99e240bd
davisjustinw <davisjustinw@gmail.com>
2019-12-18 16:01:39 -0800

    add buttons to item table

--- app/helpers/inventory_items_helper.rb
+++ app/helpers/inventory_items_helper.rb
@@ -1,12 +1,12 @@
 module InventoryItemsHelper
   def par_value( par_hash, inventory, item )
     if par_hash[item.id] && par_hash[item.id][inventory.id]
-      link_to edit_inventory_item_path( par_hash[item.id][inventory.id][:obj] ) do
+      link_to edit_inventory_item_path( par_hash[item.id][inventory.id][:obj] ), class: "button" do
         "#{par_hash[item.id][inventory.id][:par]}"
       end
     else
       link_params = { inventory_item: { inventory_id: inventory.id, item_id: item.id } }
-      link_to new_inventory_item_path( params: link_params ) do
+      link_to new_inventory_item_path( params: link_params ), class: "button" do
         "+"
       end
     end
