66db661657872b507036472484ea7b846e4b405e
davisjustinw <davisjustinw@gmail.com>
2019-12-19 17:05:02 -0800

    add breadcrumb and menu to inventory_item controller

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -1,7 +1,13 @@
 class InventoryItemsController < ApplicationController
-
   def edit
     @inventory_item = InventoryItem.find(params[:id])
+    @inventory = @inventory_item.inventory
+    @inventories = Inventory.all
+    @breadcrumbs = [
+      {label: 'Home', path: root_path},
+      {label: @inventory.name, path: inventory_path(@inventory)},
+      {label: @inventory_item.item.name, path: edit_inventory_item_path(@inventory_item)}
+    ]
   end
 
   def update
@@ -13,7 +19,13 @@ class InventoryItemsController < ApplicationController
 
   def new
     @inventory_item = InventoryItem.new(inventory_item_params)
-
+    @inventory = @inventory_item.inventory
+    @inventories = Inventory.all
+    @breadcrumbs = [
+      {label: 'Home', path: root_path},
+      {label: @inventory.name, path: inventory_path(@inventory)},
+      {label: 'New', path: new_inventory_item_path}
+    ]
   end
 
   def create
@@ -22,7 +34,6 @@ class InventoryItemsController < ApplicationController
   end
 
   private
-
   def inventory_item_params
     params.require(:inventory_item).permit(:par, :inventory_id, :item_id)
   end
